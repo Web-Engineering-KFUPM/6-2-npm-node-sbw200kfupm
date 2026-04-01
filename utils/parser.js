4d9e9cf733d3c9e13b732d82744577a46710b0e3
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-04-01 11:58:24 +0300

    fix last

--- utils/parser.js
+++ utils/parser.js
@@ -1,12 +1,12 @@
 import _ from "lodash"
 
 export function parseNumbers(input) {
-            return {numbers: _.compact(_.map(input, (str) => {
-              const num = Number(str);
-              return isNaN(num) ? null : num;
-            }))}
-          }
+  return _.compact(_.map(input, (str) => {
+    const num = Number(str);
+    return isNaN(num) ? null : num;
+  }));
+}
 
-          export function isValidOperation(operation) {
-            return _.includes(['add', 'subtract', 'multiply', 'divide'], operation)
-          }
+export function isValidOperation(operation) {
+return _.includes(['add', 'subtract', 'multiply', 'divide'], operation)
+}
