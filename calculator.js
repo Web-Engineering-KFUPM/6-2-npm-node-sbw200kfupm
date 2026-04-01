2bb0b22d16a16ce6e6a175c89795f4b4dd1bd2b2
sbw200kfupm <s202337730@kfupm.edu.sa>
2026-04-01 11:45:44 +0300

    todo 4

--- calculator.js
+++ calculator.js
@@ -244,3 +244,30 @@ switch (operation) {
 }
 
 console.log(`Result: ${result}`);
+
+export function add(numbers) {
+    return {result: numbers.reduce((acc, num) => acc + num, 0)}
+    }
+
+export function subtract(numbers) {
+  const [first, ...rest] = numbers;
+  return {
+    result: rest.reduce((acc, num) => acc - num, first)
+  };
+}
+
+export function multiply(numbers) {
+  return {result: numbers.reduce((acc, num) => acc * num, 1)}
+}
+
+export function divide(numbers) {
+  const [first, ...rest] = numbers;
+
+  if (rest.includes(0)) {
+    return { result: NaN, error: "Division by zero" };
+  }
+
+  return {
+    result: rest.reduce((acc, num) => acc / num, first)
+  };
+}
